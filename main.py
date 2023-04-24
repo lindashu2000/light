3a877d6afbce71ba51e7cdb6b4ac6670ad580be3
leolin <leolin@tencent.com>
2023-04-24 09:06:10 +0800

    m

--- main.py
+++ main.py
@@ -5,6 +5,47 @@ from get_stock_data import *
 from stock_cfg import *
 from plot import *
 
+
+@st.cache_data
+def get_option_prices(code, date):
+    ret, option_chain = quote_context.get_option_chain(code=code, start=date, end=date,
+                                                       option_type=OptionType.CALL)
+    if ret != RET_OK:
+        st.write('error:', options_expiration)
+        st.stop()
+    option_chain = option_chain.loc[(option_chain["strike_price"] > strike_price_range[0])
+                                    & (option_chain["strike_price"] < strike_price_range[1])]
+    # st.write(option_chain)
+
+    options_code = option_chain["code"].values.tolist()
+    options_strike = option_chain["strike_price"].values.tolist()
+    # st.write(options_code)
+    ret, message = quote_context.subscribe(code_list=options_code,
+                                           subtype_list=[SubType.ORDER_BOOK, TRADING_PERIOD],
+                                           subscribe_push=False)
+    if ret != RET_OK:
+        st.write('error:', message)
+        st.stop()
+
+    strike_price = list()
+    bid_price = list()
+    for index, c_option in enumerate(options_code):
+        ret, data = quote_context.get_order_book(c_option, num=3)  # 获取一次 3 档实时摆盘数据
+        if ret != RET_OK:
+            st.write('error:', data)
+            st.stop()
+        bp = float(data['Bid'][0][0])
+        ap = float(data['Ask'][0][0])
+        mp = ((bp if bp > 0 else ap) + (ap if ap > 0 else bp)) / 2
+        sp = options_strike[index]
+        if mp != 0:
+            strike_price.append(sp)
+            bid_price.append(mp)
+
+    series = pd.Series(bid_price, index=strike_price, name="price")
+    return series
+
+
 if __name__ == '__main__':
     with st.container():
         col1, col2 = st.columns([2, 2])
@@ -25,37 +66,16 @@ if __name__ == '__main__':
         'Select a range of strike price', code_cfg[code][0], code_cfg[code][1], code_cfg[code][2])
 
     # 遍历dates
-    option_chain = pd.DataFrame()
+    option_price = pd.DataFrame()
     for date in dates:
-        ret, option_chain = quote_context.get_option_chain(code=code, start=date, end=date,
-                                                           option_type=OptionType.CALL)
-        if ret != RET_OK:
-            st.write('error:', options_expiration)
-            st.stop()
-        option_chain = option_chain.loc[(option_chain["strike_price"] > strike_price_range[0])
-                                        & (option_chain["strike_price"] < strike_price_range[1])]
-        st.write(option_chain)
+        series = get_option_prices(code, date)
+        option_price[date] = series
+    st.line_chart(option_price)
 
-        options_code = option_chain["code"].values.tolist()
-        st.write(options_code)
-        ret, message = quote_context.subscribe(code_list=options_code,
-                                               subtype_list=[SubType.ORDER_BOOK, TRADING_PERIOD],
-                                               subscribe_push=False)
-        if ret != RET_OK:
-            st.write('error:', message)
-            st.stop()
-
-        strike_price = list()
-        bid_price = list()
-        for c_option in options_code:
-            ret, data = quote_context.get_order_book(c_option, num=3)  # 获取一次 3 档实时摆盘数据
-            if ret != RET_OK:
-                st.write('error:', data)
-                st.stop()
-            bp = float(data['Bid'][0][0])
-            ap = float(data['Ask'][0][0])
-            mp = ((bp if bp > 0 else ap) + (ap if ap > 0 else bp)) / 2
-            sp = int(c_option[-6:]) / 1000
-            if mp != 0:
-                strike_price.append(sp)
-                bid_price.append(mp)
+    option_t = option_price.T
+    # 遍历option_t的列名
+    stp = st.multiselect(
+        'Select you strike prices',
+        option_t.columns)
+    option_t = option_t[stp]
+    st.line_chart(option_t)
